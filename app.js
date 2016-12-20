bc47b180221278b374e804803cc6277a9a204ae5
David Stanich <dstanich@gmail.com>
2016-12-20 03:07:10 +0000

    Better error handling; spaces to tabs

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 var sensorUtil = require('./sensor-util');
 
 var GPIO_PIN = 4;
-var SECONDS_BETWEEN_READINGS = 2;
+var SECONDS_BETWEEN_READINGS = 30;
 
 
 // Loop for reading the temperature after an interval
 function runLoop(secondsToWait) {
-    setInterval(function() {
-        var result = sensorUtil.read();
-        console.log(new Date().toISOString() + ':  Temperature: ' + result.fTemp + '  Humidity: ' + result.humidity);
-    }, secondsToWait * 1000);
+	setInterval(function() {
+		try {
+			var result = sensorUtil.read();
+			console.log(new Date().toISOString() + ':  Temperature: ' + result.fTemp + '  Humidity: ' + result.humidity);
+		} catch (exception) {
+			console.error('Exception reading temp:', exception);
+		}
+	}, secondsToWait * 1000);
 }
 
 
@@ -18,8 +22,8 @@ console.log('TEMP SENSOR MONITOR');
 console.log('-------------------');
 
 if (sensorUtil.initialize(GPIO_PIN)) {
-    runLoop(SECONDS_BETWEEN_READINGS);
+	runLoop(SECONDS_BETWEEN_READINGS);
 } else {
-    console.error('Failed to initialize sensor');
-    process.exit(-1);
+	console.error('Failed to initialize sensor');
+	process.exit(-1);
 }
--- sensor-util.js
+++ sensor-util.js
@@ -1,22 +1,22 @@
 var sensorLib = require('node-dht-sensor');
 
 function initialize(gpioPin) {
-    return sensorLib.initialize(22, gpioPin);
+	return sensorLib.initialize(22, gpioPin);
 }
 
 function read() {
-    var readout = sensorLib.read();
-    var cTemp = readout.temperature.toFixed(2);
-    var fTemp = (cTemp * 1.8 + 32).toFixed(2);
-    
-    return {
-        cTemp: cTemp,
-        fTemp: fTemp,
-        humidity: readout.humidity.toFixed(2)
-    }
+	var readout = sensorLib.read();
+	var cTemp = readout.temperature.toFixed(2);
+	var fTemp = (cTemp * 1.8 + 32).toFixed(2);
+	
+	return {
+		cTemp: cTemp,
+		fTemp: fTemp,
+		humidity: readout.humidity.toFixed(2)
+	}
 }
 
 module.exports = {
-    initialize: initialize,
-    read: read
+	initialize: initialize,
+	read: read
 };
